0944036032ee37f94af5cbb23c3a363b241a8234
Erica Girges <ericafiorenza@gmail.com>
2020-09-28 13:46:03 -0700

    added error messages to GET

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.get("/", (req, res) => {
 // user data
 let users = [
     {
-        id: 1,
+        id: shortid.generate(),
         name: "Michael Scott",
         bio: "Regional Manager of the Dunder Mifflin Scranton Branch"
     }
@@ -24,8 +24,25 @@ let nextId = shortid.generate()
 
 // GET /users
 server.get("/users", (req, res) => {
-    res.status(200).json({ data: users });
+    if(users) {
+        res.status(200).json({ data: users });
+    } else {
+        res.status(500).json({ errorMessage: "The users information could not be retrieved." });
+    }
+
 });
+
+// GET /users by id
+server.get("/users/:id", (req, res) => {
+    const id = req.params.id
+    const found = users.find(user => user.id === id)
+
+    if (found) {
+        res.status(200).json({ data: found })
+    } else {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+    }
+})
 
 
 const port = 5000;
